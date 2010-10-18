7aaf77dce757b3b2f1919a1ceeec26a8386be6ae
Colomban Wendling <ban@herbesfolles.org>
2010-10-18 17:14:55 +0200

    Improve testsuite
    Add possibility to check if the output of a testsuite entry is the
    expected one, not only if the parsing succeeded.
    
    To use this feature, simply create a file named "{test_name}-output"
    containing the exact expected output (so that diff(1) would report
    no difference).

--- testsuite/tests.sh
+++ testsuite/tests.sh
@@ -13,10 +13,26 @@ echo '*************************'
 " EXIT
 
 export IFS=$'\n'
-for f in success/*; do
+for f in $(ls success/* | grep -v -e '-output$'); do
+  success=false
+  output="$f-output"
+  output_real="$(tempfile)"
+  
   echo "*** success test '$f'"
-  "${TESTPRG[@]}" "${ARGS[@]}" "$f" || exit 1
+  "${TESTPRG[@]}" "${ARGS[@]}" "$f" > "$output_real" && success=true
+  if $success; then
+    if [ -f "$output" ]; then
+      echo "  * checking output..."
+      if ! diff -u "$output" "$output_real"; then
+        echo "*** Parsing succeeded but output is not the expected one" >&2
+        success=false
+      fi
+    fi
+  fi
+  rm -f "$output_real"
+  $success || exit 1
 done
+
 for f in fail/*; do
   echo "*** fail test '$f'"
   "${TESTPRG[@]}" "${ARGS[@]}" "$f" && exit 1
