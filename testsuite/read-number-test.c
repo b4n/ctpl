b4n/ctpl

commit fd7a0a1ab0b4694e3c6dd79f75dc1e4ea433257c

Don't eat characters that will not be part of the final number
Don't eat characters that are not actually part of the finial number.
For example, don't eat the x of "0xzzz" since only the 0 by itself is
a valid number, z not being a valid hexadecimal digit.
This is not useful yet since any of these extra characters will cause
an error later on the lexing, but it would allow to have name clashes
between operators and numbers without problem.

Also refactored the code a bit to be more consistent.

We also unexpectedly won about 2% of performances, cool.

src/ctpl-input-stream.c
@@ -1077,57 +1077,72 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
                                         CtplValue       *value,
                                         GError         **error)
 {
-  gboolean  have_mantissa = FALSE;
-  gboolean  have_exponent = FALSE;
-  gboolean  have_sign     = FALSE;
-  gboolean  have_dot      = FALSE;
-  gint      read_type     = READ_BOTH;
+  gboolean  have_mantissa       = FALSE;
+  gboolean  have_exponent       = FALSE;
+  gboolean  have_exponent_delim = FALSE;
+  gboolean  have_sign           = FALSE;
+  gboolean  have_dot            = FALSE;
   GString  *gstring;
-  GError   *err = NULL;
-  gboolean  in_number = TRUE;
-  gint      base = 10;
+  GError   *err                 = NULL;
+  gboolean  in_number           = TRUE;
+  gint      base                = 10;
+  
+  #define ISSIGN(c)   ((c) == '+' || (c) == '-')
+  #define ISDIGIT(c)  ((c) >= '0' && (c) <= '9')
+  #define ISBDIGIT(c) ((c) == '0' || (c) == '1')
+  #define ISODIGIT(c) ((c) >= '0' && (c) <= '7')
+  #define ISXDIGIT(c) (ISDIGIT (c) || \
+                       ((c) >= 'a' && (c) <= 'f') || \
+                       ((c) >= 'A' && (c) <= 'F'))
   
   gstring = g_string_new ("");
   while (in_number && ! err) {
-    gchar c;
+    gchar   buf[3];
+    gssize  buf_len;
     
-    c = ctpl_input_stream_peek_c (stream, &err);
+    buf_len = ctpl_input_stream_peek (stream, buf, sizeof (buf), &err);
     if (! err) {
+      gchar c = (buf_len > 0) ? buf[0] : CTPL_EOF;
+      
       /*g_debug ("c = %c", c);*/
       switch (c) {
         case '.':
-          if (have_dot || have_exponent || ! (type & READ_FLOAT)) {
-            in_number = FALSE;
-          } else {
+          if (! have_dot && ! have_exponent_delim && (type & READ_FLOAT)) {
             g_string_append_c (gstring, c);
             have_dot = TRUE;
-            read_type = READ_FLOAT;
+            type &= READ_FLOAT;
+          } else {
+            in_number = FALSE;
           }
           break;
         
         case '+':
         case '-':
-          if ((have_mantissa || have_sign) &&
-              ! (type & READ_FLOAT && have_exponent && gstring->len > 0 &&
-                 (gstring->str[gstring->len - 1] == 'e' ||
-                  gstring->str[gstring->len - 1] == 'p'))) {
-            in_number = FALSE;
-          } else {
+          if (! have_sign && (! have_mantissa || ! have_exponent) &&
+              /* ISDIGIT() is fine here even though we probably don't know the
+               * base yet because the default base is 10 and the exponent or
+               * power are also in base 10 */
+              buf_len > 1 && ISDIGIT (buf[1])) {
             g_string_append_c (gstring, c);
             have_sign = TRUE;
+          } else {
+            in_number = FALSE;
           }
           break;
         
         case 'e':
         case 'E':
           if (base < 15) {
-            if (have_exponent || ! have_mantissa ||
-                ! (type & READ_FLOAT) || base != 10) {
-              in_number = FALSE;
-            } else {
-              have_exponent = TRUE;
-              read_type = READ_FLOAT;
+            if (have_mantissa && ! have_exponent_delim &&
+                (type & READ_FLOAT) && base == 10 &&
+                ((buf_len > 1 && ISDIGIT (buf[1])) ||
+                 (buf_len > 2 && ISSIGN (buf[1]) && ISDIGIT (buf[2])))) {
+              have_exponent_delim = TRUE;
+              have_sign = FALSE;
+              type &= READ_FLOAT;
               g_string_append_c (gstring, 'e');
+            } else {
+              in_number = FALSE;
             }
             break;
           }
@@ -1138,11 +1153,10 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
               ((gstring->len == 1 ||
                 (gstring->len == 2 && (gstring->str[0] == '+' ||
                                        gstring->str[0] == '-'))) &&
-               gstring->str[gstring->len - 1] == '0')) {
-            /*g_string_append_c (gstring, c);*/
+               gstring->str[gstring->len - 1] == '0') &&
+              buf_len > 1 && ISBDIGIT (buf[1])) {
             have_mantissa = FALSE; /* the previous 0 wasn't mantissa finally */
             base = 2;
-            read_type = READ_INT;
             type &= READ_INT;
             break;
           }
@@ -1155,7 +1169,7 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
         case 'D':
         case 'f':
         case 'F':
-          if (base < 16 || have_exponent /* exponent is decimal */) {
+          if (base < 16 || have_exponent_delim /* exponent is decimal */) {
             in_number = FALSE;
             break;
           }
@@ -1176,12 +1190,14 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
             in_number = FALSE;
             break;
           }
+          /* Fallthrough */
         case '0':
         case '1':
-          /* Fallthrough */
           g_string_append_c (gstring, c);
-          if (! have_exponent) {
+          if (! have_exponent_delim) {
             have_mantissa = TRUE;
+          } else {
+            have_exponent = TRUE;
           }
           break;
         
@@ -1191,11 +1207,11 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
               ((gstring->len == 1 ||
                 (gstring->len == 2 && (gstring->str[0] == '+' ||
                                        gstring->str[0] == '-'))) &&
-               gstring->str[gstring->len - 1] == '0')) {
+               gstring->str[gstring->len - 1] == '0') &&
+              buf_len > 1 && ISODIGIT (buf[1])) {
             /*g_string_append_c (gstring, c);*/
             have_mantissa = FALSE; /* the previous 0 wasn't mantissa finally */
             base = 8;
-            read_type = READ_INT;
             type &= READ_INT;
           } else {
             in_number = FALSE;
@@ -1204,12 +1220,16 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
         
         case 'p':
         case 'P':
-          if (! (type & READ_FLOAT) || have_exponent || base != 16) {
-            in_number = FALSE;
-          } else {
-            have_exponent = TRUE;
-            read_type = READ_FLOAT;
+          if (have_mantissa && ! have_exponent_delim &&
+              (type & READ_FLOAT) && base == 16 &&
+              ((buf_len > 1 && ISDIGIT (buf[1])) ||
+               (buf_len > 2 && ISSIGN (buf[1]) && ISDIGIT (buf[2])))) {
+            have_exponent_delim = TRUE;
+            have_sign = FALSE;
+            type &= READ_FLOAT;
             g_string_append_c (gstring, 'p');
+          } else {
+            in_number = FALSE;
           }
           break;
         
@@ -1218,7 +1238,8 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
           if ((gstring->len == 1 ||
                (gstring->len == 2 && (gstring->str[0] == '+' ||
                                       gstring->str[0] == '-'))) &&
-              gstring->str[gstring->len - 1] == '0') {
+              gstring->str[gstring->len - 1] == '0' &&
+              buf_len >= 2 && ISXDIGIT (buf[1])) {
             g_string_append_c (gstring, c);
             have_mantissa = FALSE; /* the previous 0 wasn't mantissa finally */
             base = 16;
@@ -1236,54 +1257,53 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
     }
   }
   if (! err) {
-    if (! (read_type & type)) {
+    if (! have_mantissa) {
       ctpl_input_stream_set_error (stream, &err, CTPL_IO_ERROR,
                                    CTPL_IO_ERROR_INVALID_NUMBER,
-                                   "Invalid number");
+                                   "Missing mantissa in numeric constant");
     } else {
-      read_type &= type;
-      if (! have_mantissa) {
+      gchar  *nptr = gstring->str;
+      gchar  *endptr;
+      gdouble dblval = 0.0;
+      glong   longval = 0;
+      gint    errno_save = errno;
+      
+      /*g_debug ("trying to convert '%s'", nptr);*/
+      errno = 0;
+      if (type & READ_INT) {
+        longval = strtol (nptr, &endptr, base);
+      } else {
+        dblval = g_ascii_strtod (nptr, &endptr);
+      }
+      if (! endptr || *endptr != 0) {
         ctpl_input_stream_set_error (stream, &err, CTPL_IO_ERROR,
                                      CTPL_IO_ERROR_INVALID_NUMBER,
-                                     "Missing mantissa in numeric constant");
+                                     "Invalid base %d numeric constant \"%s\"",
+                                     base, nptr);
+      } else if (errno == ERANGE) {
+        ctpl_input_stream_set_error (stream, &err, CTPL_IO_ERROR,
+                                     CTPL_IO_ERROR_RANGE,
+                                     "Overflow in numeric constant conversion");
       } else {
-        gchar  *nptr = gstring->str;
-        gchar  *endptr;
-        gdouble dblval = 0.0;
-        glong   longval = 0;
-        gint    errno_save = errno;
-        
-        /*g_debug ("trying to convert '%s'", nptr);*/
-        errno = 0;
-        if (read_type & READ_INT) {
-          longval = strtol (nptr, &endptr, base);
+        if (type & READ_INT) {
+          ctpl_value_set_int (value, longval);
         } else {
-          dblval = g_ascii_strtod (nptr, &endptr);
+          ctpl_value_set_float (value, dblval);
         }
-        if (! endptr || *endptr != 0) {
-          ctpl_input_stream_set_error (stream, &err, CTPL_IO_ERROR,
-                                       CTPL_IO_ERROR_INVALID_NUMBER,
-                                       "Invalid base %d numeric constant \"%s\"",
-                                       base, nptr);
-        } else if (errno == ERANGE) {
-          ctpl_input_stream_set_error (stream, &err, CTPL_IO_ERROR,
-                                       CTPL_IO_ERROR_RANGE,
-                                       "Overflow in numeric constant conversion");
-        } else {
-          if (read_type & READ_INT) {
-            ctpl_value_set_int (value, longval);
-          } else {
-            ctpl_value_set_float (value, dblval);
-          }
-        }
-        errno = errno_save;
       }
+      errno = errno_save;
     }
   }
   if (err) {
     g_propagate_error (error, err);
   }
   g_string_free (gstring, TRUE);
+  
+  #undef ISSIGN
+  #undef ISDIGIT
+  #undef ISBDIGIT
+  #undef ISODIGIT
+  #undef ISXDIGIT
   
   return ! err;
 }

commit a51a96570f1b91448391565cff1db20d47c3a9a2

Fix wrong reading of numbers containing signs at invalid positions
For example, don't read "41-42" with the "41-42" input, rather read
"41". This fixes reading of expressions containing plus and minus
operators not separated by blank characters.

This bug was introduced in commit fd7a0a1, so it was in v0.3.1 but not
in v0.3.

src/ctpl-input-stream.c
@@ -1118,7 +1118,8 @@ ctpl_input_stream_read_number_internal (CtplInputStream *stream,
         
         case '+':
         case '-':
-          if (! have_sign && (! have_mantissa || ! have_exponent) &&
+          if (! have_sign && (! have_mantissa ||
+                              (have_exponent_delim && ! have_exponent)) &&
               /* ISDIGIT() is fine here even though we probably don't know the
                * base yet because the default base is 10 and the exponent or
                * power are also in base 10 */

testsuite/read-number-test.c
@@ -120,6 +120,11 @@ main (int     argc,
     CHECK ("0x1ae71714",  "");
     CHECK ("0b111",       "");
     CHECK ("0b012",       "2");
+    CHECK ("42-41-1",     "-41-1");
+    CHECK ("42+41+1",     "+41+1");
+    CHECK ("42.41+1",     "+1");
+    CHECK ("42+41.1",     "+41.1");
+    CHECK ("42+e41",      "+e41");
     
     #undef CHECK
   }