e773ef2cc0ddb61a33234a349636525087d1bbb7
Colomban Wendling <ban@herbesfolles.org>
2014-11-17 17:29:12 +0100

    Fix build directory name under Windows
    Rename `build/aux` directory to `build/auxf` as `aux` is a reserved
    name under Windows.

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p build/aux          || exit 1
+mkdir -p build/auxf         || exit 1
 mkdir -p build/m4           || exit 1
 gtkdocize --flavour no-tmpl || exit 1
 autoreconf -vfi             || exit 1
